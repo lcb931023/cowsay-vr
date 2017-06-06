2206210d297431ab05c873d485dd6c3333795c08
Changbai Li <changbai.li@heliosinteractive.com>
2017-06-05 20:33:44 -0700

    WIP - porting to AR with webRTC

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 global.THREE = require('three')
 const createOrbitViewer = require('three-orbit-viewer')(THREE)
+import ArToolkitSource from './lib/artoolkitsource'
 import Cow from './lib/cow'
 
 /* Scene */
 const app = createOrbitViewer({
   clearColor: 0x000000,
-  clearAlpha: 1,
+  clearAlpha: 0,
   fov: 65,
   position: new THREE.Vector3(0, 0, 0), // camera
   target: new THREE.Vector3(0, 0, -1), // default was (0, 0, 0). pass this so it can look ahead
 })
+
+const arToolkitSource = new ArToolkitSource({
+  sourceType: 'webcam',
+})
+arToolkitSource.init(onResize)
+window.addEventListener('resize', onResize)
+function onResize() {
+  arToolkitSource.onResize()
+  arToolkitSource.copySizeTo(app.renderer.domElement)
+}
 
 const cow = new Cow()
 app.scene.add(cow)
